f29fe84239a7aa32686bc9a240cf5dd326b894a0
John B. Flester <john@flester.com>
2012-11-12 02:44:08 -0500

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,7 @@ Sampleapp::Application.configure do
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
+  config.force_ssl = true
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
