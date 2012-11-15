9d4ea8cf02019fbacfc9c8bc325b19fc26202fbe
John B. Flester <john@flester.com>
2012-11-15 10:43:27 -0500

    finish authorization with friendly forwarding, and add users index with will_paginate

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
 
   factory :user do
-    name	 "John Flester"
-	email	 "john@flester.com"
-	password "foobar"
-	password_confirmation "foobar"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}   
+    password "foobar"
+    password_confirmation "foobar"
   end
 
 end
