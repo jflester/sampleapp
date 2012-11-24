6095a30ee7e899ab9b93a5229ca94978f6929e68
John B. Flester <john@flester.com>
2012-11-24 01:51:53 -0500

    Add user microposts, start challenge of adding javascript counter to microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -38,9 +38,16 @@ describe "User pages" do
   end
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
+	let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+	let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "bar") }
     before { visit user_path(user) }
     it { should have_selector('h1',    text: user.name) }
     it { should have_selector('title', text: user.name) }
+	describe "microposts" do
+	  it { should have_content(m1.content) }
+	  it { should have_content(m2.content) }
+	  it { should have_content(user.microposts.count) }
+	end
   end
   describe "signup page" do
     before { visit signup_path }
@@ -62,10 +69,10 @@ describe "User pages" do
     end
     describe "with valid information" do
       before do
-        fill_in "Name",         with: "John Flester"
-        fill_in "Email",        with: "john@flester.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Name",         	with: "John Flester"
+        fill_in "Email",        	with: "john@flester.com"
+        fill_in "Password",         with: "foobar"
+        fill_in "Confirm Password", with: "foobar"
       end
 	  describe "after saving the user" do
 	    before { click_button submit }
